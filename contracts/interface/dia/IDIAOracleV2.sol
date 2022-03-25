89c27f8f9871cb3c9af5fe6820a6767f70fcf560
a <a@example.com>
2022-03-26 02:00:50 +0900

    add DiaPriceFeed test

--- contracts/interface/dia/IDIAOracleV2.sol
+++ contracts/interface/dia/IDIAOracleV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.7.4;
+pragma solidity 0.7.6;
 
 interface IDIAOracleV2 {
     function getValue(string memory key) external view returns (uint128, uint128);
--- test/DiaPriceFeed.spec.ts
+++ test/DiaPriceFeed.spec.ts
@@ -2,39 +2,39 @@ import { MockContract, smock } from "@defi-wonderland/smock"
 import { expect } from "chai"
 import { parseEther } from "ethers/lib/utils"
 import { ethers, waffle } from "hardhat"
-import { BandPriceFeed, TestStdReference, TestStdReference__factory } from "../typechain"
+import { DiaPriceFeed, TestDIAOracleV2, TestDIAOracleV2__factory } from "../typechain"
 
-interface BandPriceFeedFixture {
-    bandPriceFeed: BandPriceFeed
-    bandReference: MockContract<TestStdReference>
-    baseAsset: string
+interface DiaPriceFeedFixture {
+    diaPriceFeed: DiaPriceFeed
+    diaOracleV2: MockContract<TestDIAOracleV2>
+    oracleKey: string
 }
 
-async function bandPriceFeedFixture(): Promise<BandPriceFeedFixture> {
-    const testStdReferenceFactory = await smock.mock<TestStdReference__factory>("TestStdReference")
-    const testStdReference = await testStdReferenceFactory.deploy()
+async function diaPriceFeedFixture(): Promise<DiaPriceFeedFixture> {
+    const testDIAOracleV2Factory = await smock.mock<TestDIAOracleV2__factory>("TestDIAOracleV2")
+    const testDIAOracleV2 = await testDIAOracleV2Factory.deploy()
 
-    const baseAsset = "ETH"
-    const bandPriceFeedFactory = await ethers.getContractFactory("BandPriceFeed")
-    const bandPriceFeed = (await bandPriceFeedFactory.deploy(testStdReference.address, baseAsset, 900)) as BandPriceFeed
+    const oracleKey = "ETH/USD"
+    const diaPriceFeedFactory = await ethers.getContractFactory("DiaPriceFeed")
+    const diaPriceFeed = (await diaPriceFeedFactory.deploy(testDIAOracleV2.address, oracleKey, 900)) as DiaPriceFeed
 
-    return { bandPriceFeed, bandReference: testStdReference, baseAsset }
+    return { diaPriceFeed, diaOracleV2: testDIAOracleV2, oracleKey }
 }
 
-describe("BandPriceFeed/CumulativeTwap Spec", () => {
+describe("DiaPriceFeed/CumulativeTwap Spec", () => {
     const [admin] = waffle.provider.getWallets()
     const loadFixture: ReturnType<typeof waffle.createFixtureLoader> = waffle.createFixtureLoader([admin])
-    let bandPriceFeed: BandPriceFeed
-    let bandReference: MockContract<TestStdReference>
+    let diaPriceFeed: DiaPriceFeed
+    let diaOracleV2: MockContract<TestDIAOracleV2>
     let currentTime: number
     let roundData: any[]
 
     async function updatePrice(price: number, forward: boolean = true): Promise<void> {
-        roundData.push([parseEther(price.toString()), currentTime, currentTime])
-        bandReference.getReferenceData.returns(() => {
+        roundData.push([parseEther(price.toString()), currentTime])
+        diaOracleV2.getValue.returns(() => {
             return roundData[roundData.length - 1]
         })
-        await bandPriceFeed.update()
+        await diaPriceFeed.update()
 
         if (forward) {
             currentTime += 15
@@ -44,9 +44,9 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
     }
 
     beforeEach(async () => {
-        const _fixture = await loadFixture(bandPriceFeedFixture)
-        bandReference = _fixture.bandReference
-        bandPriceFeed = _fixture.bandPriceFeed
+        const _fixture = await loadFixture(diaPriceFeedFixture)
+        diaOracleV2 = _fixture.diaOracleV2
+        diaPriceFeed = _fixture.diaPriceFeed
         roundData = []
     })
 
@@ -56,16 +56,16 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
         })
 
         it("update price once", async () => {
-            roundData.push([parseEther("400"), currentTime, currentTime])
-            bandReference.getReferenceData.returns(() => {
+            roundData.push([parseEther("400"), currentTime])
+            diaOracleV2.getValue.returns(() => {
                 return roundData[roundData.length - 1]
             })
 
-            expect(await bandPriceFeed.update())
-                .to.be.emit(bandPriceFeed, "PriceUpdated")
+            expect(await diaPriceFeed.update())
+                .to.be.emit(diaPriceFeed, "PriceUpdated")
                 .withArgs(parseEther("400"), currentTime, 0)
 
-            const observation = await bandPriceFeed.observations(0)
+            const observation = await diaPriceFeed.observations(0)
             const round = roundData[0]
             expect(observation.price).to.eq(round[0])
             expect(observation.timestamp).to.eq(round[1])
@@ -75,13 +75,13 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
         it("update price twice", async () => {
             await updatePrice(400, false)
 
-            roundData.push([parseEther("440"), currentTime + 15, currentTime + 15])
-            bandReference.getReferenceData.returns(() => {
+            roundData.push([parseEther("440"), currentTime + 15])
+            diaOracleV2.getValue.returns(() => {
                 return roundData[roundData.length - 1]
             })
-            await bandPriceFeed.update()
+            await diaPriceFeed.update()
 
-            const observation = await bandPriceFeed.observations(1)
+            const observation = await diaPriceFeed.observations(1)
             const round = roundData[roundData.length - 1]
             expect(observation.price).to.eq(round[0])
             expect(observation.timestamp).to.eq(round[1])
@@ -93,18 +93,18 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
         it.skip("force error, the second update is the same timestamp", async () => {
             await updatePrice(400, false)
 
-            roundData.push([parseEther("440"), currentTime, currentTime])
-            bandReference.getReferenceData.returns(() => {
+            roundData.push([parseEther("440"), currentTime])
+            diaOracleV2.getValue.returns(() => {
                 return roundData[roundData.length - 1]
             })
-            await expect(bandPriceFeed.update()).to.be.revertedWith("CT_IT")
+            await expect(diaPriceFeed.update()).to.be.revertedWith("CT_IT")
         })
     })
 
     describe("twap", () => {
         beforeEach(async () => {
             // `base` = now - _interval
-            // bandReference's answer
+            // diaOracleV2's answer
             // timestamp(base + 0)  : 400
             // timestamp(base + 15) : 405
             // timestamp(base + 30) : 410
@@ -122,45 +122,45 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
 
         describe("getPrice", () => {
             it("return latest price if interval is zero", async () => {
-                const price = await bandPriceFeed.getPrice(0)
+                const price = await diaPriceFeed.getPrice(0)
                 expect(price).to.eq(parseEther("410"))
             })
 
             it("twap price", async () => {
-                const price = await bandPriceFeed.getPrice(45)
+                const price = await diaPriceFeed.getPrice(45)
                 expect(price).to.eq(parseEther("405"))
             })
 
-            it("asking interval more than bandReference has", async () => {
-                const price = await bandPriceFeed.getPrice(46)
+            it("asking interval more than diaOracleV2 has", async () => {
+                const price = await diaPriceFeed.getPrice(46)
                 expect(price).to.eq(parseEther("405"))
             })
 
-            it("asking interval less than bandReference has", async () => {
-                const price = await bandPriceFeed.getPrice(44)
+            it("asking interval less than diaOracleV2 has", async () => {
+                const price = await diaPriceFeed.getPrice(44)
                 expect(price).to.eq("405113636363636363636")
             })
 
             it("asking interval less the timestamp of the latest observation", async () => {
-                const price = await bandPriceFeed.getPrice(14)
+                const price = await diaPriceFeed.getPrice(14)
                 expect(price).to.eq(parseEther("410"))
             })
 
-            it("the latest band reference data is not being updated to observation", async () => {
+            it("the latest oracle data is not being updated to observation", async () => {
                 currentTime += 15
                 await updatePrice(415)
 
                 // (415 * 15 + 410 * 30) / 45 = 411.666666
-                const price = await bandPriceFeed.getPrice(45)
+                const price = await diaPriceFeed.getPrice(45)
                 expect(price).to.eq("411666666666666666666")
             })
 
             it("given variant price period", async () => {
-                roundData.push([parseEther("420"), currentTime + 30, currentTime + 30])
+                roundData.push([parseEther("420"), currentTime + 30])
                 await ethers.provider.send("evm_setNextBlockTimestamp", [currentTime + 50])
                 await ethers.provider.send("evm_mine", [])
                 // twap price should be ((400 * 15) + (405 * 15) + (410 * 45) + (420 * 20)) / 95 = 409.736
-                const price = await bandPriceFeed.getPrice(95)
+                const price = await diaPriceFeed.getPrice(95)
                 expect(price).to.eq("409736842105263157894")
             })
 
@@ -170,7 +170,7 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
 
                 // latest update time is base + 30, but now is base + 145 and asking for (now - 45)
                 // should return the latest price directly
-                const price = await bandPriceFeed.getPrice(45)
+                const price = await diaPriceFeed.getPrice(45)
                 expect(price).to.eq(parseEther("410"))
             })
         })
@@ -191,20 +191,20 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
         })
 
         it("verify status", async () => {
-            expect(await bandPriceFeed.currentObservationIndex()).to.eq(254)
+            expect(await diaPriceFeed.currentObservationIndex()).to.eq(254)
 
             // observations[255] shouldn't be updated since we only run 255 times in for loop
-            const observation255 = await bandPriceFeed.observations(255)
+            const observation255 = await diaPriceFeed.observations(255)
             expect(observation255.price).to.eq(0)
             expect(observation255.priceCumulative).to.eq(0)
             expect(observation255.timestamp).to.eq(0)
 
-            const observation254 = await bandPriceFeed.observations(254)
+            const observation254 = await diaPriceFeed.observations(254)
             expect(observation254.price).to.eq(parseEther("654"))
             expect(observation254.timestamp).to.eq(currentTimeBefore + 15 * 254)
 
             // (654 * 15 + 653 * 15 + 652 * 15) / 45 = 653
-            const price = await bandPriceFeed.getPrice(45)
+            const price = await diaPriceFeed.getPrice(45)
             expect(price).to.eq(parseEther("653"))
         })
 
@@ -216,10 +216,10 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
             // which will override the first observation which is observations[0]
             await updatePrice(beginPrice + 256)
 
-            expect(await bandPriceFeed.currentObservationIndex()).to.eq(0)
+            expect(await diaPriceFeed.currentObservationIndex()).to.eq(0)
 
             // (656 * 15 + 655 * 15 + 654 * 15) / 45 = 655
-            const price = await bandPriceFeed.getPrice(45)
+            const price = await diaPriceFeed.getPrice(45)
             expect(price).to.eq(parseEther("655"))
         })
 
@@ -229,10 +229,10 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
                 await updatePrice(beginPrice + 256 + i)
             }
 
-            expect(await bandPriceFeed.currentObservationIndex()).to.eq(9)
+            expect(await diaPriceFeed.currentObservationIndex()).to.eq(9)
 
             // (665 * 15 + 664 * 15 + 663 * 15) / 45 = 664
-            const price = await bandPriceFeed.getPrice(45)
+            const price = await diaPriceFeed.getPrice(45)
             expect(price).to.eq(parseEther("664"))
         })
 
@@ -243,10 +243,10 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
             // this one will override the first observation which is observations[0]
             await updatePrice(beginPrice + 256, false)
 
-            expect(await bandPriceFeed.currentObservationIndex()).to.eq(0)
+            expect(await diaPriceFeed.currentObservationIndex()).to.eq(0)
 
             // (((401 + 655) / 2) * 3825 + 656 * 1 ) / 3,826 = 528.0334553058
-            const price = await bandPriceFeed.getPrice(255 * 15 + 1)
+            const price = await diaPriceFeed.getPrice(255 * 15 + 1)
             expect(price).to.eq("528033455305802404600")
         })
 
@@ -257,27 +257,27 @@ describe("BandPriceFeed/CumulativeTwap Spec", () => {
             // this one will override the first observation which is observations[0]
             await updatePrice(beginPrice + 256, false)
 
-            expect(await bandPriceFeed.currentObservationIndex()).to.eq(0)
+            expect(await diaPriceFeed.currentObservationIndex()).to.eq(0)
 
             // the longest interval = 255 * 15 = 3825, it should be revert when interval > 3826
             // here, we set interval to 3827 because hardhat increases the timestamp by 1 when any tx happens
-            await expect(bandPriceFeed.getPrice(255 * 15 + 2)).to.be.revertedWith("CT_NEH")
+            await expect(diaPriceFeed.getPrice(255 * 15 + 2)).to.be.revertedWith("CT_NEH")
         })
     })
 
     describe("price is not updated yet", () => {
         beforeEach(async () => {
-            roundData.push([parseEther("100"), currentTime, currentTime])
-            bandReference.getReferenceData.returns(() => {
+            roundData.push([parseEther("100"), currentTime])
+            diaOracleV2.getValue.returns(() => {
                 return roundData[roundData.length - 1]
             })
         })
         it("get spot price", async () => {
-            await expect(bandPriceFeed.getPrice(900)).to.be.revertedWith("CT_ND")
+            await expect(diaPriceFeed.getPrice(900)).to.be.revertedWith("CT_ND")
         })
 
         it("force error, get twap price", async () => {
-            await expect(bandPriceFeed.getPrice(900)).to.be.revertedWith("CT_ND")
+            await expect(diaPriceFeed.getPrice(900)).to.be.revertedWith("CT_ND")
         })
     })
 })
