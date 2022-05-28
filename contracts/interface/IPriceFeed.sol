1181b8ca4e5b7be27b270981262918df16ae8ca2
takamiml <91838137+takami-ml@users.noreply.github.com>
2022-05-28 21:17:55 +0900

    Remove twap related code from IPriceFeed

--- contracts/interface/IPriceFeed.sol
+++ contracts/interface/IPriceFeed.sol
@@ -2,13 +2,8 @@
 pragma solidity 0.7.6;
 
 interface IPriceFeed {
-    /// @dev Returns the cached index price of the token.
-    /// @param interval The interval represents twap interval.
-    function cacheTwap(uint256 interval) external returns (uint256);
-
     function decimals() external view returns (uint8);
 
     /// @dev Returns the index price of the token.
-    /// @param interval The interval represents twap interval.
-    function getPrice(uint256 interval) external view returns (uint256);
+    function getPrice() external view returns (uint256);
 }
