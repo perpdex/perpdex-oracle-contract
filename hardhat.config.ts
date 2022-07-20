4cd32bcfbb5c0873837f3c8909c8306824c00ac8
a <a@example.com>
2022-07-20 09:18:57 +0900

    solc version 0.8.12

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,7 +14,7 @@ dotenvConfig({ path: resolve(__dirname, "./.env") })
 
 const config: HardhatUserConfig = {
     solidity: {
-        version: "0.7.6",
+        version: "0.8.12",
         settings: {
             optimizer: { enabled: true, runs: 200 },
             evmVersion: "berlin",
